00212f00aad24c8fae3b0884e79f207b24d0e82e
Nine-H <nine.gentooman@gmail.com>
2016-11-22 00:14:31 +1100

    cmake installs gsettings schemas, initial gschema support

---
+++ build/CMakeFiles/apollo11.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "src/hello_houston.c"
+  "src/header.c"
+  "src/window.c"
+  "src/apollo11.c"
+  "apollo11_valac.stamp"
+  "CMakeFiles/apollo11.dir/src/hello_houston.c.o"
+  "CMakeFiles/apollo11.dir/src/header.c.o"
+  "CMakeFiles/apollo11.dir/src/window.c.o"
+  "CMakeFiles/apollo11.dir/src/apollo11.c.o"
+  "apollo11.pdb"
+  "apollo11"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/apollo11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
